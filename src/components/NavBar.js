4e3f29e3e4f60fb91d098347d9eff9c6c71820c5
Franco Yunes <franco.yunes@hotmail.com>
2022-12-30 01:10:45 -0300

    :art: Remove unnecesary BS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,17 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div><nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#"><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058148734501924994/calculadora.png' /></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-item nav-link active" href="https://www.linkedin.com/in/francoyunes/"><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058183862183809084/linkedin.png'/></a>
-        <a class="nav-item nav-link active" href="https://github.com/fyunes/"><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058184365319929946/github-sign.png'/></a>
-      </div>
+    
+      <nav className='navBar'>
+      <ul>
+    <li><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058148734501924994/calculadora.png' alt='Calculadora'/></li>    
+    <div className="social">
+    <li> <a href="https://www.linkedin.com/in/francoyunes/"><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058183862183809084/linkedin.png' alt='Linkedin'/></a> </li> 
+    <li>  <a href="https://github.com/fyunes/"><img className='logo' src='https://cdn.discordapp.com/attachments/781224167524925460/1058184365319929946/github-sign.png' alt='Github'/></a> </li>          
     </div>
-  </nav></div>
+    </ul>
+  </nav>
+  
   )
 }
 
